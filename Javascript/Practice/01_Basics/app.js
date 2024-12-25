ce13a67b2be1c16f96995ded5f3fe4ce234176c7
Saya1206 <sayali.patil0831>
2024-12-25 20:41:58 +0530

    JS Basics

---
+++ Javascript/Practice/01_Basics/app.js
@@ -0,0 +1,20 @@
+console.log("Hello World");
+
+console.log(10 + 2);
+
+console.log("Good Morning" + " Everyone!");
+
+console.warn("This is warning");
+
+console.error("This is an error");
+
+// Comments
+
+//Single Line Comments: Single line comments start with //
+
+//Multi-line Comments: Multi-line comments start with /* and end with */
+
+/*
+
+
+*/
