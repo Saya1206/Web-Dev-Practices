e0a500b2878c0762ba44a567de20eba9901aa2b1
Saya1206 <sayali.patil0831>
2024-12-26 22:15:47 +0530

    JS Boolean

---
+++ Javascript/Practice/04_Boolean/app.js
@@ -0,0 +1,10 @@
+console.log(9 > 4);
+console.log(typeof true);
+console.log(9 == 9.0);
+console.log(5 != 5);
+
+// == checks only value
+console.log(9 == "9");
+
+// === checks both type & value
+console.log(9 === "9");
