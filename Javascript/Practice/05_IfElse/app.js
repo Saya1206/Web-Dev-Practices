d62cab73a437a54a20e55a91d8e1f4031137d0a3
Saya1206 <sayali.patil0831>
2024-12-26 22:34:48 +0530

    JS if-else

---
+++ Javascript/Practice/05_IfElse/app.js
@@ -0,0 +1,35 @@
+// if
+
+/*
+let age = 20;
+
+if (age > 18) {
+  console.log("You can Drive");
+}
+  */
+
+// if-else
+
+let age = 14;
+
+if (age > 18) {
+  console.log("You can Drive");
+} else {
+  console.log("Use a Bicycle");
+}
+
+// if-else ladder
+
+let marks = 76;
+
+if (marks >= 90) {
+  console.log("A Grade");
+} else if (marks >= 70 && marks < 90) {
+  console.log("B Grade");
+} else if (marks >= 50 && marks < 70) {
+  console.log("C Grade");
+} else if (marks >= 30 && marks < 50) {
+  console.log("D Grade");
+} else {
+  console.log("Fail");
+}
