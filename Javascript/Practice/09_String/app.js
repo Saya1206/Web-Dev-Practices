c79c12d11f21d039d923b688e1c6a382198ebc2f
Saya1206 <sayali.patil0831>
2025-01-11 23:00:53 +0530

    JS String

---
+++ Javascript/Practice/09_String/app.js
@@ -0,0 +1,10 @@
+let s1 = "Hello";
+let s2 = "Hello, JS!";
+let s3 = `Welcome in JS tutorial`;
+
+console.log(s1);
+console.log(s2);
+console.log(s3);
+
+let msg = new String("Good morning everyone!");
+console.log(msg);
