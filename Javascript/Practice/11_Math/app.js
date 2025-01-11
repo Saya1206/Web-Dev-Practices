4a0d7b57041c67927d05b7c54c97312dba7f43ac
Saya1206 <sayali.patil0831>
2025-01-11 23:46:38 +0530

    JS Math

---
+++ Javascript/Practice/11_Math/app.js
@@ -0,0 +1,26 @@
+console.log(Math.PI);
+
+console.log(Math.pow(2, 3));
+
+console.log(Math.sqrt(81));
+
+console.log(Math.abs(-5));
+
+console.log(Math.ceil(2.4));
+console.log(Math.ceil(3.8));
+
+console.log(Math.floor(1.9));
+console.log(Math.floor(2.3));
+
+console.log(Math.round(3.8));
+console.log(Math.round(2.3));
+
+console.log(Math.max(10, 37, 23, 6));
+
+console.log(Math.min(10, 37, 3, 23, 6));
+
+console.log(Math.random());
+
+console.log(Math.random() * 11);
+
+console.log(Math.log(1));
